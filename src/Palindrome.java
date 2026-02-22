2e17342e68561bd8deb3c0e35636a9cd76ed1012
sam-gonzales <samantha.gonzales@sjsu.edu>
2026-02-21 20:17:29 -0800

    Update Palindrome.java, added comments

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,4 +1,5 @@
 public class Palindrome {
+    // to hold the reference to the left node
     static class leftSide {
       Node left;
 
@@ -9,22 +10,24 @@ public class Palindrome {
     
     public static boolean isPalindrome(Node head) {
         leftSide left = new leftSide(head);
+        // recursively checking if linked list is a palindrome
         return isPalindromeRec(left, head);
     }
     
     public static boolean isPalindromeRec(leftSide left, Node right){
+        // base case when we reach the end 
         if(right == null){
             return true;
         }
-        
+        //traverse list from the end to the start of the list
         if(!isPalindromeRec(left, right.next)){
             return false;
         }
-        
+        // compare left and right keys
         if(left.left.key != right.key){
             return false;
         }
-        
+        // move left one
         left.left = left.left.next;
         
         return true;
